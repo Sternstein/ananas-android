661aa9c0a706fdca7d021289b477bb106fad9fec
Sternstein <andreygagarinchel@gmail.com>
2016-04-21 15:25:48 +0500

    Reader activity created

--- app/src/main/java/com/example/andreas/ananas/MainActivity.java
+++ app/src/main/java/com/example/andreas/ananas/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.andreas.ananas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     String[] names = {"Тест 1","Test 2","Test 3","Test 4" };
-
+    ListView lvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,33 @@ public class MainActivity extends AppCompatActivity {
 
         // присваиваем адаптер списку
         lvMain.setAdapter(adapter);
+
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                TextView textView = (TextView) view;
+                String strText = textView.getText().toString(); // получаем текст нажатого элемента
+
+                if(strText.equalsIgnoreCase(getResources().getString(R.string.test))) {
+
+                    startActivity(new Intent(getApplicationContext(), Readtext.class));
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
 }
