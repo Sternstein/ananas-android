a56b9fd79c72b5e33f729b2cfa5a0c9bec019582
Sternstein <andreygagarinchel@gmail.com>
2016-05-06 13:51:56 +0500

    New Menu Activity added

--- app/src/main/java/com/example/andreas/ananas/MainActivity.java
+++ app/src/main/java/com/example/andreas/ananas/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.andreas.ananas;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import org.json.JSONArray;
@@ -21,64 +23,26 @@ import java.net.URL;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.squareup.picasso.Picasso;
+
 
 public class MainActivity extends AppCompatActivity {
     public static String LOG_TAG = "my_log";
-    String[] names = {"Тест 1","Test 2","Test 3","Test 4" };
-    String[] names2 = {"Вкусная пицца","Гречка","Коты атакуют людей","Свежий хлеб уже в продаже" };
-    ListView lvMain;
+    int leng;
+
+    String[] titleb;
+    String[] bodyb;
+    String[] picb;
+    TextView tV;
+    ImageView iv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new ParseTask().execute();
-
-        // находим список
-        ListView lvMain = (ListView) findViewById(R.id.listView);
-
-        // создаем адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, names2);
-
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-         //       R.layout.mystyle, R.id.label, names);
-
-
-        // присваиваем адаптер списку
-        lvMain.setAdapter(adapter);
-
-        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                TextView textView = (TextView) view;
-                String strText = textView.getText().toString(); // получаем текст нажатого элемента
-
-                if(position == 0) {
-
-                    startActivity(new Intent(getApplicationContext(), Readtext.class));
-                }
-
-                if(position == 1) {
-
-                    startActivity(new Intent(getApplicationContext(), list.class));
-                }
-                if(position == 2) {
-
-                    startActivity(new Intent(getApplicationContext(), list2.class));
-                }
-
-            }
-        });
-
-
-
-
-
-
-
-
-
+        ImageView iv = (ImageView) findViewById(R.id.imV);
+        TextView tV = (TextView) findViewById(R.id.tV);
     }
 
     private class ParseTask extends AsyncTask<Void, Void, String> {
@@ -87,11 +51,14 @@ public class MainActivity extends AppCompatActivity {
         BufferedReader reader = null;
         String resultJson = "";
 
+
+
+
         @Override
         protected String doInBackground(Void... params) {
             // получаем данные с внешнего ресурса
             try {
-                URL url = new URL("http://192.168.1.7/site/3.json");
+                URL url = new URL("http://192.168.1.7/site/4.json");
 
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -127,23 +94,40 @@ public class MainActivity extends AppCompatActivity {
             try {
                 dataJsonObj = new JSONObject(strJson);
                 JSONArray info = dataJsonObj.getJSONArray("info");
+                leng = info.length();
 
-                for (int i = 0; i < info.length(); i++) {
+
+                for (int i = 0; i < leng; i++) {
                     JSONObject friend = info.getJSONObject(i);
 
 
-                    String phone = friend.getString("title");
-                    String email = friend.getString("body");
-                    String skype = friend.getString("pic");
+                    String title = friend.getString("title");
+                    String body = friend.getString("body");
+                    String pic = friend.getString("pic");
 
-                    Log.d(LOG_TAG, "Title " + phone);
-                    Log.d(LOG_TAG, "Body: " + email);
-                    Log.d(LOG_TAG, "PicUr: " + skype);
+                    titleb[i] = title;
+                    bodyb[i] = body;
+                    picb[i]= pic;
+                    Log.d(LOG_TAG, titleb[i]);
+                    Log.d(LOG_TAG, bodyb[i]);
+                    Log.d(LOG_TAG, picb[i]);
                 }
+
+
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+
+           // tV.setText("Download is finished!");
+           // Intent intent = new Intent(MainActivity.this, menu.class);
+           // Bundle ka = new Bundle();
+           // ka.putStringArray ("tit",titleb);
+           // ka.putStringArray ("bod",bodyb);
+           // ka.putStringArray ("pic",picb);
+           // intent.putExtras(ka);
+           // startActivity(intent);
+
         }
     }
 
---
+++ app/src/main/java/com/example/andreas/ananas/menu.java
@@ -0,0 +1,101 @@
+package com.example.andreas.ananas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class menu extends AppCompatActivity {
+
+    public String[] titleb;
+    public String[] bodyb;
+    public String[] picb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Intent intent = getIntent();
+        Bundle bb = getIntent().getExtras();
+        titleb = bb.getStringArray("tit");
+        bodyb = bb.getStringArray("bod");
+        picb = bb.getStringArray("pic");
+
+        // находим список
+        ListView lvMain = (ListView) findViewById(R.id.listView3);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, titleb);
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        //       R.layout.mystyle, R.id.label, names);
+
+
+        // присваиваем адаптер списку
+        lvMain.setAdapter(adapter);
+
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                TextView textView = (TextView) view;
+                String strText = textView.getText().toString(); // получаем текст нажатого элемента
+
+                if(position == 0) {
+
+                    String titl = titleb[0];
+                    String bod = bodyb[0];
+                    String pi = picb[0];
+                    Intent intent = new Intent(getApplicationContext(), list2.class);
+                    intent.putExtra("tit", titl);
+                    intent.putExtra("bo", bod);
+                    intent.putExtra("p", pi);
+                    startActivity(intent);
+                }
+
+                if(position == 1) {
+
+                    String titl = titleb[1];
+                    String bod = bodyb[1];
+                    String pi = picb[1];
+                    Intent intent2 = new Intent(getApplicationContext(), list2.class);
+                    intent2.putExtra("tit", titl);
+                    intent2.putExtra("bo", bod);
+                    intent2.putExtra("p", pi);
+                    startActivity(intent2);
+                }
+                if(position == 2) {
+
+                    String titl = titleb[2];
+                    String bod = bodyb[2];
+                    String pi = picb[2];
+                    Intent intent3 = new Intent(getApplicationContext(), list2.class);
+                    intent3.putExtra("tit", titl);
+                    intent3.putExtra("bo", bod);
+                    intent3.putExtra("p", pi);
+                    startActivity(intent3);
+
+
+
+                }
+                if(position == 3) {
+
+                    Intent intent4 = new Intent(getApplicationContext(), Readtext.class);
+                    startActivity(intent4);
+                }
+                if(position == 4) {
+
+                    startActivity(new Intent(getApplicationContext(), list.class));
+                }
+
+
+            }
+        });
+    }
+}
